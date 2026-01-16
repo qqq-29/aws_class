53ba2e22d734e0d6904194325802f4a48110769f
qqq-29 <qjc159357896@gmail.com>
2026-01-16 18:20:36 +0900

    spring boot : boot

--- spring boot/boot/src/main/java/kr/hi/boot/controller/CommentController.java
+++ spring boot/boot/src/main/java/kr/hi/boot/controller/CommentController.java
@@ -1,14 +1,21 @@
 package kr.hi.boot.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.hi.boot.model.dto.CommentResponseDTO;
+import kr.hi.boot.model.util.Criteria;
+import kr.hi.boot.model.util.CustomUser;
+import kr.hi.boot.model.util.PageMaker;
 import kr.hi.boot.model.vo.Comment;
 import kr.hi.boot.service.CommentService;
 
@@ -24,14 +31,56 @@ public class CommentController {
 	}
 	
 	@GetMapping("/posts/{num}/comments")
-	public ResponseEntity<List<Comment>> getComments(
+	public ResponseEntity<CommentResponseDTO> getComments(
 			//화면에서 보낸 게시글 번호를 가져옴
-	        @PathVariable("num") int poNum) {
-		//서비스에게 게시글 번호를 주면서 댓글 목록 가져오라고 요청
+	        @PathVariable("num") int poNum,
+	        //화면에서 보낸 페이지 정보를 가져옴
+	        Criteria cri) {
+		
+		//한페이에 댓글 5개로 설정
+		cri.setPerPageNum(5);
+		//서비스에게 게시글 번호와 페이지 정보를 주면서 댓글 목록 가져오라고 요청
 		//댓글목록 = 서비스야.댓글목록가져와(게시글번호);
-		List<Comment> list =commentService.getcomments(poNum);
-		//가져온 댓글 목록을 화면에 전달
-	    return ResponseEntity.ok(list);
+		List<Comment> list =commentService.getcomments(poNum, cri);
+		
+		//서비스에게 게시글 번호와 페이지 정보를 주면서 PageMaker 객체를 가져오라고 요청
+		//PageMaker객체 = 서비스야.PageMaker객체가져와(게시글번호, 페이지정보)
+		PageMaker pm = commentService.getPageMaker(poNum, cri);
+		
+		//가져온 댓글 목록과 페이지네이션정보를 화면에 전달
+		CommentResponseDTO dto = new CommentResponseDTO(list, pm);
+	    return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping("/posts/{num}/comments")
+	public ResponseEntity<String> getCommentsPost(
+			//- 화면에서 보낸 게시글 번호를 가져옴
+				@PathVariable("num")int postNum,
+				//- 화면에서 보낸 댓글 내용을 가져옴
+				@RequestBody Comment comment,
+				//- 로그인한 사용자 정보 가져옴
+				@AuthenticationPrincipal CustomUser user
+				) {
+		
+		//- 서비스에게 게시글번호와 댓글내용, 사용자정보를 주면서 댓글을 등록하고 결과를 문자열로 가져오라고 요청
+		comment.setPostNum(postNum);
+		
+		//결과 = 서비스야.댓글등록해(게시글번호와댓글내용, 사용자정보);
+		String result = commentService.insertComment(comment, user);
+		//- 가져온 결과를 화면에 전송
+		return ResponseEntity.ok(result); 
+	}
+	@DeleteMapping("/posts/{postNum}/comments/{comments}")
+	public ResponseEntity<String> postsCommentsDelete(
+			//- 화면에서 보낸 댓글 번호를 가져옴
+				@PathVariable("comments")int coNum,
+			//- 로그인한 사용자 정보를 가져옴
+				@AuthenticationPrincipal CustomUser user
+			){
+		//- 서비스에게 댓글 번호와 로그인한 사용자 정보를 주면서 댓글을 삭제하고 결과를 알려달라고 요청
+		String result = commentService.deleteComment(coNum, user);
+		//- 결과를 화면에 전달
+		return ResponseEntity.ok(result); 
 	}
 
 }
--- spring boot/boot/src/main/java/kr/hi/boot/service/CommentService.java
+++ spring boot/boot/src/main/java/kr/hi/boot/service/CommentService.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import kr.hi.boot.dao.CommentDAO;
+import kr.hi.boot.model.dto.CommentResponseDTO;
+import kr.hi.boot.model.util.Criteria;
+import kr.hi.boot.model.util.CustomUser;
+import kr.hi.boot.model.util.PageMaker;
 import kr.hi.boot.model.vo.Comment;
 
 @Service
@@ -16,13 +20,83 @@ private final CommentDAO commentDAO;
 		this.commentDAO = commentDAO;
 	}
 
-	public List<Comment> getcomments(int poNum) {
-		//다오에게 게시글 번호를 주면서 댓글 목록을 가져오라고 요청
+	public List<Comment> getcomments(int poNum, Criteria cri) {
+		//다오에게 게시글 번호와 페이지 정보를 주면서 댓글 목록을 가져오라고 요청
 		//게시글 목록=다오야.댓글목록가져와(게시글번호);
-		List<Comment> list = commentDAO.selectComments(poNum);
+		List<Comment> list = commentDAO.selectComments(poNum, cri);
 		
 		//가져온 댓글 목록을 반환
 		return list;
+	}
+
+	public PageMaker getPageMaker(int poNum, Criteria cri) {
+		//한페이지네이션에서 최대 페이지수를 3개로 선언
+		int pageCount = 3;
+		//다오에게 게시글번호를 주면서 전체 댓글 수를 가져오라고 요청
+		//전체댓글수 = 디오야.전체댓글수가져와(게시글번호, 현재페이지);
+		int count = commentDAO.selectCommentsCount(poNum);
+		//최대 페이지수, 전체게시글수, 현재페이지 정보를 이용하여 PageMaker 객체를 생성
+		PageMaker pm = new PageMaker(pageCount, cri, count);
+		//생성된 객체를 반환
+		return pm;
+	}
+
+	public String insertComment(Comment comment, CustomUser user) {
+		//- 사용자가 로그인 안했으면 "로그인이 필요한 서비스입니다."라고 반환
+		if(user == null) {
+			return "로그인이 필요한 서비스입니다.";
+		}
+		//댓글 내용이 비었으면 "댓글을 입력하세요."라고 반환
+		if(comment.getContent() == null ||
+				comment.getContent().isBlank()) {
+			return "댓글을 입력하세요.";
+		}
+		
+		//- 다오에게 게시글 번호, 댓글 내용, 사용자아이디를 주면서 게시글을 등록하라고 요청후 등록 여부를 알려달라고 요청
+		comment.setId(user.getUsername());
+		//등록결과 = 다오야.게시글등록해(댓글정보);
+		boolean result = commentDAO.insertComment(comment);
+		//- 등록했으면 "댓글을 등록했습니다."라고 반환하고
+		if(result) {
+			return "댓글을 등록했습니다.";
+		}
+		//- 못했으면 댓글을 등록하지 못했습니다라고 반환
+		return null;
+		
+	}
+
+	public String deleteComment(int coNum, CustomUser user) {
+		//- 사용자가 로그인되어 있지 않으면 "로그인이 필요한 서비스입니다" 반환
+		if(user == null) {
+			return "로그인이 필요한 서비스입니다.";
+		}
+		//  - 댓글작성자와 사용자아이디가 다르면 "작성자가 아닙니다"를 반환
+		String id = user.getUsername();
+		if(!isWriter(coNum, id)) {
+			return "작성자가 아닙니다";
+		}
+		
+		//- 다오에게 댓글 번호를 주면서 삭제하고 결과를 알려달라고 요청
+		boolean result = commentDAO.deleteComment(coNum);
+		//- 삭제했으면 "댓글을 삭제했습니다"를 반환
+		if(result) {
+			return "댓글을 삭제했습니다.";
+		}
+		//- 못했으면 "댓글을 삭제하지 못했습니다"를 반환
+		
+		return "댓글을 삭제하지 못했습니다";
+	}
+	private boolean isWriter(int coNum, String id) {
+		//  - 다오에게 댓글번호 주면서 댓글 정보 가져오라고 요청
+		Comment comment = commentDAO.selectComment(coNum);
+		
+		//- 가져온 댓글 정보가 없거나 댓글작성자와 사용아이디가 다르면
+		//false를 반환
+		if(comment == null || !comment.getId().equals(id)) {
+			return false;
+		}
+		//아니면 true를 반환
+		return true;
 	}
 
 
